dd34112409d040aaa247c7b1c5d8c99289f913ab
chrisapx <mcaplexyc@gmail.com>
2025-01-04 15:33:39 +0300

    added listing card template

---
+++ src/client/components/global/ListingCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListingCard = ({ item }) => {
+  return (
+    <div>
+      This is the card to hold item information
+      // image, name, etc
+    </div>
+  )
+}
+
+export default ListingCard
