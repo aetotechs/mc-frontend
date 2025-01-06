2d7332dc1505a64e7d657203a8c0997ddbec7296
chrisapx <mcaplexyc@gmail.com>
2025-01-06 12:32:38 +0300

    cleaned up

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  // <AuthProvider>
-  // </AuthProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
