40acc4f15df3d1a701d3000a945199e4252655dd
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2025-01-06 13:23:24 +0300

    Header

--- src/client/components/global/Header.jsx
+++ src/client/components/global/Header.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-[4vw] py-4">
+    <div className="flex justify-between px-[8vw] py-4">
       <div>
         <img src="/logos/mycrib.png" width={100} />
       </div>
 
       <div>
-        <ul className="flex gap-3 items-center">
+        <ul className="flex gap-5 items-center">
           <li>Explore</li>
 
           <li>Manage Rentals</li>
--- src/client/components/global/Hero.jsx
+++ src/client/components/global/Hero.jsx
@@ -1,36 +1,37 @@
 const Hero = () => {
   return (
-    <div className="bg-hero bg-cover  bg-center h-[70vh] flex justify-center items-center flex-col gap-5">
-        
-      <h3 className="font-extrabold text-[50px] leading-10 text-white">Discover the Easiest Way to Rent</h3>
+    <div className="bg-hero bg-cover relative  bg-center h-[70vh] flex justify-center items-center flex-col gap-5">
+              <div className="absolute inset-0 bg-black opacity-30"></div>
+
+      <h3 className="font-extrabold text-[50px] leading-[60px] text-white">Discover the Easiest Way to Rent</h3>
 
       <div className="bg-white flex items-center justify-between rounded-[50px] py-3 px-4 divide-x gap-3">
-        <div className="flex flex-col">
+        <div className="flex flex-col px-4">
           <span className="font-medium text-sm">Location</span>
           <span>
             <span></span>
-            <span className="font-medium text-base ">Search city or area</span>
+            <span className="font-medium text-[15px] text-[#8B8D98]">Search city or area</span>
           </span>
         </div>
-        <div className="flex flex-col">
-          <span>Location</span>
+        <div className="flex flex-col px-4">
+          <span className="font-medium text-sm">Property</span>
           <span>
             <span></span>
-            <span>Search city or area</span>
+            <span className="font-medium text-[15px] text-[#8B8D98]">Select property type</span>
           </span>
         </div>
-        <div className="flex flex-col">
-          <span>Location</span>
+        <div className="flex flex-col px-4">
+          <span className="font-medium text-sm">Price</span>
           <span>
             <span></span>
-            <span>Search city or area</span>
+            <span className="font-medium text-[15px] text-[#8B8D98]">Choose price range</span>
           </span>
         </div>
-        <div className="flex flex-col">
-          <span>Location</span>
+        <div className="flex flex-col px-4">
+          <span className="font-medium text-sm">Location</span>
           <span>
             <span></span>
-            <span>Search city or area</span>
+            <span className="font-medium text-[15px] text-[#8B8D98]">Search city or area</span>
           </span>
         </div>
       </div>
