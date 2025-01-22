b71eb5b341e33241767c101feda48f1b4a54b233
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2025-01-22 16:01:59 +0300

    Created Acccounts Page

--- src/client/pages/Account.jsx
+++ src/client/pages/Account.jsx
@@ -20,13 +20,27 @@ const AccountSettings = () => {
         <Header />
       </section>
 
-      <section className="border-b pb-3 px-[8vw] my-10">
+      <section className="border-b pb-3 px-[8vw] my-5">
         <span className="text-xl font-semibold text-[#000000E5]">Account</span>
       </section>
 
       <section className="grid grid-cols-2 px-[8vw] gap-5">
-        <div className="border col-span-2 py-6 flex justify-between px-3 border-[#D8D9E0] rounded-xl">
-          <div></div>
+        <div className="border col-span-2 py-6 flex items-center justify-around border-[#D8D9E0] rounded-xl">
+          <div className="flex items-center gap-6">
+            <div className="h-24 w-24 rounded-full object-cover overflow-hidden relative">
+              <img src="/images/user.png" alt="User" />
+            </div>
+            <div className="absolute border translate-x-20 translate-y-7 border-white rounded-full p-1 bg-[#2F91D7]">
+              <Edit02Icon className="h-3 w-3 text-white" />
+            </div>
+            <div className="flex flex-col">
+              <span className="font-semibold text-[20px]">John Doe</span>
+              <span className="text-[#62636C] text-[15px]">@john123</span>
+              <span className="text-[#62636C] text-[15px]">
+                johndoe@gmail.com
+              </span>
+            </div>
+          </div>
           <div className="flex flex-col gap-2">
             <div className="flex items-center gap-2">
               <Location01Icon className="h-4 w-4" />
@@ -68,13 +82,14 @@ const AccountSettings = () => {
           </div>
         </div>
         <div className="border col-span-1 py-6 flex justify-between px-3 border-[#D8D9E0] rounded-xl">
-          
-        <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1">
             <PaymentSuccess02Icon className="h-9 w-9" />
 
             <div className="flex flex-col">
               <span className="font-semibold">Payment Methods</span>
-              <span className="text-[#62636C]">Securely manage your preferred payment methods.</span>
+              <span className="text-[#62636C]">
+                Securely manage your preferred payment methods.
+              </span>
             </div>
           </div>
           <div>
@@ -87,15 +102,17 @@ const AccountSettings = () => {
           </div>
         </div>
         <div className="border col-span-1 py-6 flex justify-between px-3 border-[#D8D9E0] rounded-xl">
-        <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1">
             <Notification01Icon className="h-9 w-9" />
 
             <div className="flex flex-col">
               <span className="font-semibold">Notifications</span>
-              <span className="text-[#62636C]">All your important updates in one place.</span>
+              <span className="text-[#62636C]">
+                All your important updates in one place.
+              </span>
             </div>
           </div>
-       
+
           <div>
             <Button className="bg-[#2F91D7] text-white text-sm p-1 gap-1">
               <span>
