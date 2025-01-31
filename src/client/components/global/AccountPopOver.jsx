33edddcdff3c6c98c8a5b5fe4e45ee1481784f2e
chrisapx <mcaplexyc@gmail.com>
2025-02-01 01:48:26 +0300

    edited the account pop-over

--- src/client/components/global/AccountPopOver.jsx
+++ src/client/components/global/AccountPopOver.jsx
@@ -2,6 +2,7 @@ import {  useState } from "react";
 import { CircleUserRound } from "lucide-react";
 import { useAuthDialog } from "../../utils/hooks/useAuthDialog";
 import { dialog_operations } from "../../utils/constansts/DialogOperations";
+import { getAuthUser, isAuthenticated, logout } from "../../utils/cookies/AuthCookiesManager";
 
 export function AccountPopover() {
   const { openDialog } = useAuthDialog();
@@ -10,6 +11,11 @@ export function AccountPopover() {
   const toggleTooltip = () => {
     setIsTooltipVisible(!isTooltipVisible);
   };
+
+  const handleLogout = () => {
+    logout();
+    toggleTooltip();
+  }
   return (
     <>
       <div
@@ -20,12 +26,12 @@ export function AccountPopover() {
         <span>
           <CircleUserRound className="h-5 w-5" />
         </span>
-        <span>Account</span>
+        <span>{ getAuthUser()?.username || "Account"}</span>
       </div>
       {isTooltipVisible && (
         <div className="absolute flex flex-col w-52 text-sm bg-white text-black rounded-md px-2 py-1 top-[60px] z-30  transform -translate-x-1/2 shadow-md">
           <div className="absolute -top-1 left-[80%] transform -translate-x-1/2 w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[6px] border-b-white"></div>
-          <div className="flex flex-col gap-1.5 font-normal p-2 text-sm">
+          { !isAuthenticated() ? <div className="flex flex-col gap-1.5 font-normal p-2 text-sm">
             <span
               className="cursor-pointer "
               onClick={() => {
@@ -45,7 +51,18 @@ export function AccountPopover() {
               Login
             </span>
             <span>Help / Support</span>
+          </div> 
+          : 
+          <div className="p-2 grid gap-4">
+            <p className="">Hello {getAuthUser()?.lastName} 😊</p>
+            <p className="flex justify-between bg-gray-100 hover:bg-gray-300 hover:text-blue-600 cursor-pointer p-2 rounded-md"
+              onClick={handleLogout}
+              >
+                Logout 
+              <span className="pi pi-sign-out text-sm"/>
+            </p>
           </div>
+          }
         </div>
       )}
     </>
