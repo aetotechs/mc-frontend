7c2a5494bd893b4310c1f6106bfec96abdc28756
chrisapx <mcaplexyc@gmail.com>
2025-02-19 02:28:52 +0300

    added progress panel

--- src/partner/pages/NewProperty.jsx
+++ src/partner/pages/NewProperty.jsx
@@ -1,21 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PartnerHeader from '../components/PartnerHeader'
 import Footer from '../../client/components/global/Footer'
 import { ArrowLeft02Icon } from 'hugeicons-react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const NewProperty = () => {
+  const navogate = useNavigate();
+  const [step, setStep] = useState(1);
+
+  const handleNext = () => {
+    if(step < 5) setStep(step + 1);
+  }
+
+  const handlePrevious = () => {
+    if(step > 1) setStep(step - 1);
+  }
+
   return (
-    <div>
+    <div className='relative h-screen'>
       <section className='sticky top-0 z-50'>
         <PartnerHeader bottomBorder />
       </section>
 
       <section className='px-[8vw] py-[2vw] flex items-center gap-6'>
-        <ArrowLeft02Icon/>
-        <article className='font-bold text-lg'>Add property</article>
+        <Link to={-1}>
+          <ArrowLeft02Icon/>
+        </Link>
+        <article className='font-bold text-xl'>Add property</article>
       </section>
 
       {/* Forms and size bar */}
+      <section className='mx-[8vw] flex gap-8'>
+        <section className='bg-blue-50 p-6 text-sm w-[20vw] space-y-6 rounded-lg'>
+          <div className={`text-gray-400`}>
+            <p className={`${step === 1 && 'text-primary'}`}>Step 1/5</p>
+            <p className={`${step === 1 && 'text-black'}`}>Basic information</p>
+          </div>
+          <div className={`text-gray-400`}>
+            <p className={`${step === 2 && 'text-primary'}`}>Step 2/5</p>
+            <p className={`${step === 2 && 'text-black'}`}>Add Units</p>
+          </div>
+          <div className={`text-gray-400`}>
+            <p className={`${step === 3 && 'text-primary'}`}>Step 3/5</p>
+            <p className={`${step === 3 && 'text-black'}`}>Property Features</p>
+          </div>
+          <div className={`text-gray-400`}>
+            <p className={`${step === 4 && 'text-primary'}`}>Step 4/5</p>
+            <p className={`${step === 4 && 'text-black'}`}>Upload Media</p>
+          </div>
+          <div className={`text-gray-400`}>
+            <p className={`${step === 5 && 'text-primary'}`}>Step 5/5</p>
+            <p className={`${step === 5 && 'text-black'}`}>Preview</p>
+          </div>
+        </section>
+        <section className=''>
+          section 2
+        </section>
+      </section>
 
       <section className='pt-[4vh]'>
         <Footer/>
--- src/partner/pages/Properties.jsx
+++ src/partner/pages/Properties.jsx
@@ -23,10 +23,10 @@ const Properties = () => {
       </section>
 
       <section className='px-[8vw] py-[2vw] flex justify-between'>
-        <article className='font-bold text-lg'>properties</article>
+        <article className='font-bold text-xl'>Properties</article>
         <section className='flex gap-4'>
             <article className='flex items-center border rounded-lg'>
-                <Search01Icon size={32} className='p-2' onClick={() => {console.log("Triger search event here")}}/>
+                <Search01Icon size={28} className='p-2' onClick={() => {console.log("Triger search event here")}}/>
                 <input type="search" name="" placeholder='Search' id="" className='flex-1 h-full rounded-r-lg focus:border-0'/>
             </article>
             <Button onClick={() => navigate('/new') } className='bg-primary text-white gap-2 font-semibold p-2 text-xs'>
@@ -42,8 +42,8 @@ const Properties = () => {
             <section className='flex itemx-center justify-center pb-3'>
                 <Home11Icon size={52} className='text-center bg-blue-100 p-2 rounded-full text-primary'/>
             </section>
-            <p className='font-bold text-center text-sm'>No properties added yet</p>
-            <p className='text-xs text-center text-gray-500'>Click '+ New' to get started</p>
+            <p className='font-bold text-center text-xl'>No properties added yet</p>
+            <p className='text-md text-center text-gray-500'>Click <a href="/new" className='text-primary'>'+ New'</a> to get started</p>
         </article>
       </section>
 
