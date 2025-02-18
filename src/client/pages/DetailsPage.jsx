be8d18e239b21ed1ef35c15019c2e1ba05ea5a0d
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2025-02-18 10:01:21 +0300

    Ratings

--- src/client/pages/DetailsPage.jsx
+++ src/client/pages/DetailsPage.jsx
@@ -3,6 +3,9 @@ import Header from "../components/global/Header";
 import { useAuthDialog } from "../utils/hooks/useAuthDialog";
 import { dialog_operations } from "../utils/constansts/DialogOperations";
 import Footer from "../components/global/Footer";
+
+import { ProgressBar } from "primereact/progressbar";
+
 import { Button } from "primereact/button";
 import {
   Call02Icon,
@@ -98,11 +101,11 @@ const DetailsPage = () => {
             <span>What’s Available (4 units)</span>
           </div>
 
-          <div>
-            <span>Amenities</span>
+          <div className="my-3">
+            <span className="">Amenities</span>
 
             <div className="grid grid-cols-3 ">
-              <div>
+              <div className="space-y-4">
                 <div className="flex items-center gap-1">
                   <span>
                     <Sofa01Icon />
@@ -133,7 +136,7 @@ const DetailsPage = () => {
                 </div>
               </div>
 
-              <div>
+              <div className="space-y-4">
                 <div className="flex items-center gap-1">
                   <span>
                     <Sofa01Icon />
@@ -164,7 +167,7 @@ const DetailsPage = () => {
                 </div>
               </div>
 
-              <div>
+              <div className="space-y-4">
                 <div className="flex items-center gap-1">
                   <span>
                     <Sofa01Icon />
@@ -192,6 +195,101 @@ const DetailsPage = () => {
                   </span>
 
                   <span>Gym</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-[50%]">
+            <span>Features</span>
+
+            <div className="flex justify-between">
+              <div>
+                <span>View</span>
+                <ol className="list-disc list-inside">
+                  <li>Water</li>
+                  <li>Mountain</li>
+                </ol>
+              </div>
+
+              <div>
+                <span>Pets</span>
+                <ol className="list-disc">
+                  <li>Dogs allowed</li>
+                  <li>Cats allowed</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col ">
+            <span>Explore the area</span>
+            <span>Wakiso, Uganda</span>
+
+            <div className="border rounded-md w-[90vw]">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31918.13613198205!2d32.49461968053299!3d0.2905604467530591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177da322867754d1%3A0x56da7cad63130f35!2sWakiso%2C%20Kampala!5e0!3m2!1sen!2sug!4v1739857757719!5m2!1sen!2sug"
+                width="750"
+                height="250"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
+
+          <div className="my-4">
+            <span>Reviews</span>
+
+            <div>
+              <div className="border shadow-md rounded-md w-[30%] p-3 my-3">
+                <span>Ratings</span>
+
+                <div className="flex justify-between">
+                  <span>4.1</span>
+
+                  <div className="flex flex-col">
+                    <span>Based on 80 ratings</span>
+                  </div>
+                </div>
+                <div className="border h-1 my-3 w-full bg-slate-400"></div>
+
+                <div>
+                  <div className="flex">
+                    <span>5</span>
+                    <span>
+                     
+                      <ProgressBar value={50}></ProgressBar>
+                    </span>
+                  </div>
+                  <div className="flex">
+                    <span>4</span>
+                    <span>
+                     
+                      <ProgressBar value={50}></ProgressBar>
+                    </span>
+                  </div>
+                  <div className="flex">
+                    <span>3</span>
+                    <span>
+             
+                      <ProgressBar value={50}></ProgressBar>
+                    </span>
+                  </div>
+                  <div className="flex">
+                    <span>2</span>
+                    <span>
+                
+                      <ProgressBar value={50}></ProgressBar>
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <span>1</span>
+                    <span className="grow">
+                   
+                      <ProgressBar value={50}  className="h-2"></ProgressBar>
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
